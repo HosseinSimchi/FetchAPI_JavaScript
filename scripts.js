e0cb4e8e42a3a1403cb20ccb34b5005b3917f634
Hossein Simchi <95399810+HosseinSimchi@users.noreply.github.com>
2022-06-16 18:23:54 +0430

    the second try has been failed

--- scripts.js
+++ scripts.js
@@ -1,13 +1,40 @@
-var url = 'https://www.omdbapi.com/?i=tt3896198&apikey=a7e684d8';
 
-let response = fetch(url);
-fetch(url)
-    .then(response => {
-        console.log(response.json());
-    })
-    .catch(error => {
-        console.log(error);
-    });
+// const moviesList = [];
+
+// const getMovies = async () => {
+//   let page = 1;
+//   let API_KEY = 'a7e684d8'
+//   while (page <= 3) {
+//     const res = await fetch(
+//       `https://www.omdbapi.com/?apikey=${API_KEY}&s=*&type=movie&page=${page}`
+//     );
+//     const movies = await res.json();
+//     movies.Search.forEach((movie) => moviesList.push(movie));
+//     page++;
+//   }
+//   console.log({ moviesList });
+// };
+
+// getMovies();
+// api url
+const api_url =
+	//"https://www.omdbapi.com/?i=tt3896198&apikey=a7e684d8";
+    "http://www.omdbapi.com/?apikey=a7e684d8&s=*&page=1"
+
+// Defining async function
+async function getapi(url) {
+	
+	// Storing response
+	const response = await fetch(url);
+	
+	// Storing data in form of JSON
+	var data = await response.json();
+    console.log(data);
+}
+
+getapi(api_url)
+
+
 
 
 
